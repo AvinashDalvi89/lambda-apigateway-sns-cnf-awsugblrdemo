d2765d43b51abdbfffe4329ac1dd3127eab8ffb1
aviboy2006 <avianshsdalvi@gmail.com>
2021-02-19 17:06:35 +0530

    Push initial commit for demo lambda code and common errors

---
+++ lambda-sample/index.js
@@ -0,0 +1,41 @@
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    var params = {
+      Bucket: 'angularbuildbucket',
+      Fields: {
+        key: '1003000265.pdf'
+      }
+    };
+    try {
+        s3.createPresignedPost(params, (err, data) => {
+            body = data;
+        });
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
